a58588d07ccfcfa2a775700476d0369716dccdfc
schn0025 <arthur.schneider@etudiant.univ-reims.fr>
2023-01-11 09:15:39 +0100

    Ajout de la fonction getAnnotationGrilleDemineur : pas de test

--- Model/GrilleDemineur.py
+++ Model/GrilleDemineur.py
@@ -30,7 +30,7 @@ def type_grille_demineur(grille: list) -> bool:
     if nc == 0:
         return False
     return next(filterfalse(lambda line: type(line) == list and len(line) == nc
-                            and next(filterfalse(type_cellule, line), True) is True, grille), True) is True
+                                         and next(filterfalse(type_cellule, line), True) is True, grille), True) is True
     # Tableau régulier
     # nc = None
     # for line in grille:
@@ -51,6 +51,7 @@ def type_grille_demineur(grille: list) -> bool:
     #         return False
     # return True
 
+
 def construireGrilleDemineur(nbLigne: int, nbCol: int) -> list:
     """
     prend un nombre de ligne et de collone en parametre et renvoi une grille pour le demineur
@@ -59,17 +60,20 @@ def construireGrilleDemineur(nbLigne: int, nbCol: int) -> list:
     :return: la grille de dimension voulue
     """
     if type(nbLigne) != int or type(nbCol) != int:
-        raise TypeError(f"construireGrilleDemineur : Le nombre de lignes {nbLigne} ou de colonnes {nbCol} n’est pas un entier.")
+        raise TypeError(
+            f"construireGrilleDemineur : Le nombre de lignes {nbLigne} ou de colonnes {nbCol} n’est pas un entier.")
     if nbLigne <= 0 or nbCol <= 0:
-        raise ValueError(f"construireGrilleDemineur : Le nombre de lignes {nbLigne} ou de colonnes {nbCol} est négatif ou nul.")
-    grille=[]
+        raise ValueError(
+            f"construireGrilleDemineur : Le nombre de lignes {nbLigne} ou de colonnes {nbCol} est négatif ou nul.")
+    grille = []
     for i in range(nbLigne):
-        ligne=[]
+        ligne = []
         for y in range(nbCol):
             cellule = construireCellule()
             ligne.append(cellule)
         grille.append(ligne)
     return grille
+
 
 def getNbLignesGrilleDemineur(grille: list) -> int:
     """
@@ -79,10 +83,11 @@ def getNbLignesGrilleDemineur(grille: list) -> int:
     """
     if not type_grille_demineur(grille):
         raise TypeError("getNbLignesGrilleDemineur : Le paramètre n’est pas une grille")
-    nbLigne= len(grille)
+    nbLigne = len(grille)
     return nbLigne
 
-def getNbColonnesGrilleDemineur(grille: list)->int:
+
+def getNbColonnesGrilleDemineur(grille: list) -> int:
     """
     renvoi le nombre de collone de la grille
     :param grille: grille de demineur
@@ -90,11 +95,11 @@ def getNbColonnesGrilleDemineur(grille: list)->int:
     """
     if not type_grille_demineur(grille):
         raise TypeError("getNbColonnesGrilleDemineur : Le paramètre n’est pas une grille")
-    nbCol=len(grille[0])
+    nbCol = len(grille[0])
     return nbCol
 
 
-def isCoordonneeCorrecte(grille: list, co: tuple)-> bool:
+def isCoordonneeCorrecte(grille: list, co: tuple) -> bool:
     """
     recheche ci les coordonnée sont bien dans la grille
     :param grille: grille du demineur
@@ -105,9 +110,11 @@ def isCoordonneeCorrecte(grille: list, co: tuple)-> bool:
         raise TypeError("isCoordonneeCorrecte : un des paramètres n’est pas du bon type.")
     ligne = getLigneCoordonnee(co)
     col = getColonneCoordonnee(co)
-    return ligne < getNbLignesGrilleDemineur(grille) and ligne >= 0 and col < getNbColonnesGrilleDemineur(grille) and col >= 0
+    return ligne < getNbLignesGrilleDemineur(grille) and ligne >= 0 and col < getNbColonnesGrilleDemineur(
+        grille) and col >= 0
 
-def getCelluleGrilleDemineur(grille: list,co:tuple) -> dict:
+
+def getCelluleGrilleDemineur(grille: list, co: tuple) -> dict:
     """
     cheche la cellule au coordonnée donnée et la renvoi
     :param grille: grille: grille du demineur
@@ -121,7 +128,8 @@ def getCelluleGrilleDemineur(grille: list,co:tuple) -> dict:
     cellule = grille[ligne][col]
     return cellule
 
-def getContenuGrilleDemineur(grille: list, co: tuple)-> int:
+
+def getContenuGrilleDemineur(grille: list, co: tuple) -> int:
     """
     return le contenus de la cellule au coordonnée passer en paramètre
     :param grille: grille du demineur
@@ -131,7 +139,8 @@ def getContenuGrilleDemineur(grille: list, co: tuple)-> int:
     cellule = getCelluleGrilleDemineur(grille, co)
     return getContenuCellule(cellule)
 
-def setContenuGrilleDemineur(grille: list, co: tuple, contenu)-> None:
+
+def setContenuGrilleDemineur(grille: list, co: tuple, contenu) -> None:
     """
     modifie le contenu de la cellule au coordonnée passer en paramètre
     :param grille: grille du demineur
@@ -139,10 +148,11 @@ def setContenuGrilleDemineur(grille: list, co: tuple, contenu)-> None:
     :param contenu: le contenus que l'on doit remplacer
     """
     cellule = getCelluleGrilleDemineur(grille, co)
-    setContenuCellule(cellule,contenu)
+    setContenuCellule(cellule, contenu)
     return None
 
-def isVisibleGrilleDemineur(grille: list, co: tuple)-> bool:
+
+def isVisibleGrilleDemineur(grille: list, co: tuple) -> bool:
     """
     indique ci la cellule au coordonnée passer en paramètre est visible ou non
     :param grille: grille du demineur
@@ -150,6 +160,7 @@ def isVisibleGrilleDemineur(grille: list, co: tuple)-> bool:
     """
     cellule = getCelluleGrilleDemineur(grille, co)
     return isVisibleCellule(cellule)
+
 
 def setVisibleGrilleDemineur(grille: list, co: tuple, visibili: bool) -> None:
     """
@@ -162,14 +173,16 @@ def setVisibleGrilleDemineur(grille: list, co: tuple, visibili: bool) -> None:
     setVisibleCellule(cellule, visibili)
     return None
 
-def contientMineGrilleDemineur(grille: list, co : tuple) -> bool:
+
+def contientMineGrilleDemineur(grille: list, co: tuple) -> bool:
     """
     detecte la presence d'une mine dans la cellule placer au coordonnée passer en paramètre
     :param grille: grille du demineur
     :param co: coordonnée recherche dans la grille
     :return: True si il y a une bombe dans la cellule et false sinon
     """
-    return getContenuGrilleDemineur(grille,co) == const.ID_MINE
+    return getContenuGrilleDemineur(grille, co) == const.ID_MINE
+
 
 def getCoordonneeVoisinsGrilleDemineur(grille: list, co: tuple) -> list:
     """
@@ -178,7 +191,7 @@ def getCoordonneeVoisinsGrilleDemineur(grille: list, co: tuple) -> list:
     :param co: coordonnée dans la grille
     :return: la liste des voissin de la co passer en paramètre
     """
-    if not type_grille_demineur(grille) and not isCoordonneeCorrecte(grille,co):
+    if not type_grille_demineur(grille) and not isCoordonneeCorrecte(grille, co):
         raise TypeError("getCoordonneeVoisinsGrilleDemineur : un des paramètres n’est pas du bon type")
     if not isCoordonneeCorrecte(grille, co):
         raise IndexError("getCoordonneeVoisinsGrilleDemineur : la coordonnée n’est pas dans la grille.")
@@ -204,15 +217,16 @@ def getCoordonneeVoisinsGrilleDemineur(grille: list, co: tuple) -> list:
                 lstVoisin.append(coTemp)
     return lstVoisin
 
-def placerMinesGrilleDemineur(grille: list, nb: int, co :tuple) -> None:
+
+def placerMinesGrilleDemineur(grille: list, nb: int, co: tuple) -> None:
     """
     place nb mine aleatoirement
     :param grille: grille du demineur
     :param nb: nombre de mines a placer
     :param co: coordonnée d'une cellule dans la grille qui ne doit pas être une mine
     """
-    ligneMax = getNbLignesGrilleDemineur(grille) -1
-    colMax = getNbColonnesGrilleDemineur(grille) -1
+    ligneMax = getNbLignesGrilleDemineur(grille) - 1
+    colMax = getNbColonnesGrilleDemineur(grille) - 1
     if nb < 0 or nb >= ((ligneMax + 1) * (colMax + 1)):
         raise ValueError("placerMinesGrilleDemineur : Nombre de bombes à placer incorrect")
 
@@ -229,6 +243,7 @@ def placerMinesGrilleDemineur(grille: list, nb: int, co :tuple) -> None:
             setContenuGrilleDemineur(grille, coTemp, const.ID_MINE)
             nbMinePlace += 1
     compterMinesVoisinesGrilleDemineur(grille)
+
 
 def compterMinesVoisinesGrilleDemineur(grille: list) -> None:
     """
@@ -248,7 +263,13 @@ def compterMinesVoisinesGrilleDemineur(grille: list) -> None:
                         nbMine += 1
                 setContenuGrilleDemineur(grille, co, nbMine)
 
+
 def getNbMinesGrilleDemineur(grille: list) -> int:
+    """
+    compt le nombre de mine restant sur le demineur
+    :param grille: grille du demineur
+    :return: le nombre de mine restante
+    """
     if not type_grille_demineur(grille):
         raise ValueError("getNbMinesGrilleDemineur : le paramètre n’est pas une grille")
     nbMine = 0
@@ -260,3 +281,15 @@ def getNbMinesGrilleDemineur(grille: list) -> int:
             if getContenuGrilleDemineur(grille, co) == const.ID_MINE:
                 nbMine += 1
     return nbMine
+
+
+def getAnnotationGrilleDemineur(grille: list, co: tuple) -> str:
+    """
+    cherche l'annotation d'une cellule a partire d'une grille et d'une co
+    :param grille: grille du demineur
+    :param co: coordonnée d'une cellule dans la grille qui ne doit pas être une mine
+    :return: l'annotation trouver
+    """
+    cel = getCelluleGrilleDemineur(grille, co)
+    anot = getAnnotationCellule(cel)
+    return anot
